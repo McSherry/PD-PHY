5da403a72940ba4b1537a2117c700f2da9d74ae0
Liam McSherry <mcsherry.liam@gmail.com>
2020-01-21 00:41:37 +0000

    Add capture portion of BMC transmitter tests.

--- src/test/BiphaseMarkTransmitter_SingleWrite_TB.vhd
+++ src/test/BiphaseMarkTransmitter_SingleWrite_TB.vhd
@@ -40,6 +40,15 @@ architecture Impl of BiphaseMarkTransmitter_SingleWrite_TB is
         );
     end component;
     
+    component Decoder4b5b port(
+        CLK : in    std_logic;
+        WE  : in    std_logic;
+        ARG : in    std_logic_vector(4 downto 0);
+        Q   : out   std_ulogic_vector(3 downto 0);
+        K   : out   std_ulogic
+        );
+    end component;
+    
     -- Wishbone signals
     --
     -- These are named as if they were signals from a bus master, and so the
@@ -59,6 +68,11 @@ architecture Impl of BiphaseMarkTransmitter_SingleWrite_TB is
     signal DCLK     : std_logic := '0';
     signal LD_DAT_I : std_ulogic;
     signal LD_EN_I  : std_ulogic;
+    
+    -- 4b5b decoder signals
+    signal DEC_ARG  : std_logic_vector(4 downto 0) := "00000";
+    signal DEC_Q    : std_ulogic_vector(3 downto 0);
+    signal DEC_K    : std_ulogic;
     
     -- Test internal signals
     signal CaptureDone : std_logic := '0';
@@ -139,52 +153,135 @@ begin
     capture: process
         -- Tracks the current clock cycle
         variable Cycle : integer := 0;
-        variable Txt : string;
+        
+        -- Tracks the current received byte number
+        variable ByteNo : integer := 0;
         
         -- Stores the inversion state of the line. 
-        variable LInvert : std_logic;
+        variable LInvert : std_logic := '0';
+        
+        -- Aids in accessing the test data
+        variable OFFSET : integer;
+        variable ADDR   : std_logic_vector(1 downto 0);
+        variable DATA   : std_logic_vector(7 downto 0);
+        
+        -- Whether, in testing received data, we have to wait for the higher
+        -- half of the data. This is set to false when we receive a K-code.
+        variable HiWait : boolean := false;
     begin        
         -- The first thing transmitted should be a preamble of alternating
         -- ones and zeroes, BMC-coded, for 64 cycles.
-        while Cycle < 64 loop
-            Txt := "Cycle " & to_string(Cycle) & ", ";
-        
+        while Cycle < 64 loop        
             wait on DCLK;
             
             -- We don't want to proceed until the line driver has enabled its
-            -- output, as before then the output is invalid. This is only
+            -- output as before then the output is invalid. This is only
             -- relevant until we get started.
             next when Cycle = 0 and LD_EN_I = '0';
         
             -- We start on zero, and as the driver starts low this should be
             -- an actual zero as well as a logical one.
-            check_equal(LD_DAT_I, LInvert, Txt & "PRE logic 0, first half");
+            check_equal(LD_DAT_I, LInvert, "Cycle " & to_string(Cycle) & ", PRE logic 0, first half");
 
             -- As it's logic zero it's unchanging, so it should still be low
             -- output on the second edge.
             wait on DCLK;
-            check_equal(LD_DAT_I, LInvert, Txt & "PRE logic 0, second half");
+            check_equal(LD_DAT_I, LInvert, "Cycle " & to_string(Cycle) & ", PRE logic 0, second half");
             
             -- Next cycle
-            Cycle := Cycle + 1;
+            Cycle   := Cycle + 1;
             LInvert := not LInvert;
             
             
             -- The next value is a one, so we should see a maintained first
             -- half followed by an inversion on the second half.
             wait on DCLK;
-            check_equal(LD_DAT_I, LInvert, Txt & "PRE logic 1, first half");
+            check_equal(LD_DAT_I, LInvert, "Cycle " & to_string(Cycle) & ", PRE logic 1, first half");
             
             wait on DCLK;
-            check_equal(LD_DAT_I, not LInvert, Txt & "PRE logic 1, second half");
+            check_equal(LD_DAT_I, not LInvert, "Cycle " & to_string(Cycle) & ", PRE logic 1, second half");
             
             -- Next cycle
-            Cycle := Cycle + 1;
+            Cycle   := Cycle + 1;
             LInvert := not LInvert;
         end loop;
         
+        -- What should follow the preamble is the data we wrote to the
+        -- transmitter, except in 4b5b- and BMC-coded form. We'll decode the
+        -- data and compare it with our expected values.
+        while LD_EN_I = '1' loop
+            -- As above, we have to calculate offset like this to make
+            -- writing out the test data intuitive.
+            OFFSET := (NumCases - ByteNo - 1) * DatWidth;
+            
+            -- These are calculated in exactly the same way as in the
+            -- stimulus process.
+            ADDR := WrData(OFFSET + (DatWidth - 1) downto OFFSET + (DatWidth - 2));
+            DATA := WrData(OFFSET + (DatWidth - 3) downto OFFSET);
         
-        -- TODO: use the test data to automate checking
+        
+            -- The first half-UI should always remain at the inversion state of
+            -- the line, so we don't need to record it.
+            wait on DCLK;
+            check_equal(LD_DAT_I, LInvert, "Cycle " & to_string(Cycle) & ", DATA UI-start invert");
+            
+            -- If the next half-UI stays at the inversion state, we've recorded
+            -- a zero. Otherwise, we've recorded a one.  We shift the data
+            -- through the input to our 4b5b decoder.
+            --
+            -- Data is transmitted little-endian, so we shift downwards.
+            wait on DCLK;
+            DEC_ARG(4)          <= '0' when LD_DAT_I = LInvert else '1';
+            DEC_ARG(3 downto 0) <= DEC_ARG(4 downto 1);
+            
+            -- As we can only meaningfully decode data in 5-bit chunks, we have
+            -- to wait for 5 cycles before we can check the output. Decoding
+            -- will give us a 4-bit data item, so for raw data (as opposed to
+            -- K-codes) we need to compare in two halves.
+            --
+            -- However, we need to order the check for the higher half of the
+            -- first so that we can easily check for it.
+            if (Cycle - 64) mod 10 = 0 and HiWait then
+                -- As below, wait to ensure that we definitely decode correctly.
+                wait until rising_edge(WB_CLK);
+                wait until rising_edge(WB_CLK);
+                
+                check_equal(DEC_Q, DATA(7 downto 4), "RX Byte " & to_string(ByteNo) & ", higher half");
+            
+                -- Clear the "wait for higher half" flag
+                HiWait := false;
+                
+                ByteNo := ByteNo + 1;
+            
+            elsif (Cycle - 64) mod 5 = 0 then
+                -- We wait to ensure that the decoder has definitely processed
+                -- our input. The Wishbone clock should be so much faster than
+                -- the transmitter data clock that this is safe.
+                wait until rising_edge(WB_CLK);
+                wait until rising_edge(WB_CLK);
+                
+                -- Little-endian transmission means that, if we have data, it
+                -- will be the lower half of the data byte.
+                check_equal(DEC_Q, DATA(3 downto 0), "RX Byte " & to_string(ByteNo) & ", lower half");
+                
+                -- If this was data and not a K-code, we need to wait for its
+                -- higher half to arrive.
+                if DEC_K = '0' then
+                    HiWait := true;
+                    
+                -- Otherwise, if it was a K-code, we need to move onto the next
+                -- test item.
+                else
+                    ByteNo := ByteNo + 1;
+                end if;
+            end if;
+            
+            Cycle := Cycle + 1;
+        end loop;
+        
+        CaptureDone <= '1';
+        
+        wait;
     end process;
 
 
@@ -219,5 +316,12 @@ begin
         LD_DAT_O    => LD_DAT_I,
         LD_EN_O     => LD_EN_I
         );
-        
+    
+    Decoder: Decoder4b5b port map(
+        CLK => WB_CLK,
+        WE  => '1',
+        ARG => DEC_ARG,
+        Q   => DEC_Q,
+        K   => DEC_K
+        );
 end;
